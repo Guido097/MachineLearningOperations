6c2d6613b6c7c3d7dfc50fcd17bd7fc9a81d2166
Guido097 <112497761+Guido097@users.noreply.github.com>
2023-09-05 14:30:14 -0300

    first commit

--- main.py
+++ main.py
@@ -8,3 +8,27 @@ df_userdata = pd.read_csv('src/usersdata.csv')
 @app.get("/")
 async def root():
     return { ''' Hola! Este es un proyecto individual para la carrera de Data Science de Henry. Te recomiendo leer el README'''}
+
+@app.get('/')
+def userdata(User_id: str):
+    # Filtra el DataFrame para obtener los datos del usuario específico
+    user_data = df_userdata[df_userdata['User_id'] == User_id]
+
+    if user_data.empty:
+        return {"message": "Usuario no encontrado"}
+
+    # Calcula la cantidad de dinero gastado por el usuario
+    total_spent = user_data['price']
+
+    # Calcula el porcentaje de recomendación en base a reviews.recommend
+    recommendation_percentage = user_data['recommendation_percent']
+
+    # Obtiene la cantidad de elementos del usuario
+    num_items = user_data['items_count']
+
+    return {
+        "User_id": User_id,
+        "Total_spent": total_spent,
+        "Recommendation_percentage": recommendation_percentage,
+        "Num_items": num_items
+    }
