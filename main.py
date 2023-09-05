d63c5eb8289f525570e50c026771192b0cd932a1
Guido097 <112497761+Guido097@users.noreply.github.com>
2023-09-05 14:12:23 -0300

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from fastapi import FastAPI
+import pandas as pd
 
+app = FastAPI
+
+df_userdata = pd.read_csv('src/usersdata.csv')
+
+@app.get("/")
+async def root():
+    return { ''' Hola! Este es un proyecto individual para la carrera de Data Science de Henry. Te recomiendo leer el README'''}
